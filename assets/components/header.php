3b335019c4e5f96aa07dc8810863de2768593032
Christine Wong <55353492+Lillliant@users.noreply.github.com>
2025-07-30 10:43:10 -0400

    Componentalization

---
+++ assets/components/header.php
@@ -0,0 +1,41 @@
+<!-- Common site-wide header for Trekker Tours -->
+<!-- TODO: For specific pages, this header includes context-specific help articles (e.g., admin for admin pages) -->
+<header>
+    <nav class="header-navbar navbar navbar-expand-lg">
+        <a class="navbar-brand" href="#">
+            <img src="/3340/assets/img/logo.png" alt="Trekker Tours Logo" style="width:40px;">
+        </a>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/3340/index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Services</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Theme
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#" onclick="toggleTheme('dark')">Dark</a></li>
+                        <li><a class="dropdown-item" href="#" onclick="toggleTheme('light')">Light</a></li>
+                        <li><a class="dropdown-item" href="#" onclick="toggleTheme('pastel')">Pastel</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/3340/pages/login/login.php">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/3340/pages/login/register.php">Register</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
