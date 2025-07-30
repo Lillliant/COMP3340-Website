310dd74a33fe23b04da6890084dd3da16393cb0f
Christine Wong <55353492+Lillliant@users.noreply.github.com>
2025-07-30 16:42:48 -0400

    Comment out debug log for original stylesheet location in toggleTheme function

--- assets/js/toggleTheme.js
+++ assets/js/toggleTheme.js
@@ -1,6 +1,6 @@
 function toggleTheme(value) {
   let sheet = document.getElementById("themeStylesheet");
-  console.log(`Original location: ${sheet.href}`);
+  //console.log(`Original location:${sheet.href}`);
   sheet.href = `/3340/assets/css/${value}.css`;
   console.log(`Theme changed to ${sheet.href}`);
 }
