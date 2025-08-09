d74ea738954827ed66896876b72c68b0b5e16f95
Christine Wong <55353492+Lillliant@users.noreply.github.com>
2025-08-09 17:43:04 -0400

    Bug fix

--- 404.php
+++ 404.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+session_start(); // Start the session to access session variables
 ?>
 
 <!doctype html>
@@ -8,12 +8,12 @@ session_start();
 <head>
     <title>Page Not Found - Trekker Tours</title>
     <!-- Common site-wide SEO metadata for Trekker Tours -->
-    <?php include '../../assets/components/seo.php'; ?>
+    <?php include 'assets/components/seo.php'; ?>
     <meta name="description" content="404 page for Trekker Tours. The page you are looking for does not exist.">
     <meta name="keywords" content="404, page not found, trekker tours, error, missing page">
     <!-- Import layout and necessary dynamic theme change function -->
-    <?php include '../../assets/components/layout.php'; ?>
-    <script src="../../assets/js/toggleTheme.js" defer></script>
+    <?php include 'assets/components/layout.php'; ?>
+    <script src="assets/js/toggleTheme.js" defer></script>
 </head>
 
 <body>
@@ -22,6 +22,9 @@ session_start();
 
     <!-- Main Content -->
     <h1>Trekker Tours</h1>
+    <!-- Alert component for displaying messages -->
+    <h2>404 - Page Not Found</h2>
+    <?php include 'assets/components/alert.php'; ?>
     <p>Oh no, the page you are looking for does not exist!</p>
 </body>
 
